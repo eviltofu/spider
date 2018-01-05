5208637c4b74192da11a712769ad7a275cbc416d
Jerome Chan <eviltofu@gmail.com>
2018-01-05 11:19:06 +0800

    Changed identifiable to has-identity

--- src/identifiables.lisp
+++ src/identifiables.lisp
@@ -1,14 +1,14 @@
 (in-package :spider)
 
-(defclass identifiable ()
+(defclass has-identity ()
   ((uuid
     :initarg :uuid
     :initform (uuid:make-v1-uuid)
-    :accessor identifiable-uuid)))
+    :accessor has-identity-uuid)))
 
-(defgeneric equal-identifiable (i1 i2))
+(defgeneric equal-identity (i1 i2))
 
-(defmethod equal-identifiable ((i1 identifiable) (i2 identifiable))
-  (let ((i1s (princ-to-string (identifiable-uuid i1)))
-	(i2s (princ-to-string (identifiable-uuid i2))))
+(defmethod equal-identity ((i1 has-identity) (i2 has-identity))
+  (let ((i1s (princ-to-string (has-identity-uuid i1)))
+	(i2s (princ-to-string (has-identity-uuid i2))))
     (equal i1s i2s)))
